92d3626dce5c0bbedea45b57c65746965fdb7d16
Noé Sautel <noesautel@icloud.com>
2022-11-02 14:39:05 -0400

    UPD ds pipeline for sagemaker

--- src/kedro_tutorial/pipelines/data_science/pipeline.py
+++ src/kedro_tutorial/pipelines/data_science/pipeline.py
@@ -1,44 +1,31 @@
-from kedro.pipeline import Pipeline, node
-from kedro.pipeline.modular_pipeline import pipeline
+from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import evaluate_model, split_data, train_model
+from .nodes import (
+    evaluate_model,
+    split_data,
+    train_model_sagemaker,
+    untar_model,
+)
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    pipeline_instance = pipeline(
+    return pipeline(
         [
             node(
                 func=split_data,
-                inputs=["model_input_table", "params:model_options"],
-                outputs=["X_train", "X_test", "y_train", "y_test"],
-                name="split_data_node",
+                inputs=["model_input_table", "parameters"],
+                outputs=["X_train@pickle", "X_test", "y_train", "y_test"],
             ),
             node(
-                func=train_model,
-                inputs=["X_train", "y_train"],
-                outputs="regressor",
-                name="train_model_node",
+                func=train_model_sagemaker,
+                inputs=["X_train@path", "params:sklearn_estimator_kwargs"],
+                outputs="model_path",
             ),
+            node(untar_model, inputs="model_path", outputs="regressor"),
             node(
                 func=evaluate_model,
                 inputs=["regressor", "X_test", "y_test"],
                 outputs=None,
-                name="evaluate_model_node",
             ),
         ]
-    )
-    ds_pipeline_1 = pipeline(
-        pipe=pipeline_instance,
-        inputs="model_input_table",
-        namespace="active_modelling_pipeline",
-    )
-    ds_pipeline_2 = pipeline(
-        pipe=pipeline_instance,
-        inputs="model_input_table",
-        namespace="candidate_modelling_pipeline",
-    )
-    return pipeline(
-        pipe=ds_pipeline_1 + ds_pipeline_2,
-        inputs="model_input_table",
-        namespace="data_science",
     )
